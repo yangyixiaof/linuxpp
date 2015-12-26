77fc5ac91ef6eb20e869686dc9cb2d85c10f261b
yangyixiaof <yangyixiaofirst@163.com>
2015-12-26 21:57:15 +0800

    aas

---
+++ linuxprogramprocessor/src/main/java/cn/yyx/research/linuxprogramprocessor/ProgramProcessorRoot.java
@@ -0,0 +1,66 @@
+package cn.yyx.research.linuxprogramprocessor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import cn.yyx.research.language.Utility.BigDirectory;
+import cn.yyx.research.language.Utility.SourceCodeFileIteration;
+
+public class ProgramProcessorRoot {
+
+	String processdir = null;
+	Thread runThread = null;
+	File dir = null;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public ProgramProcessorRoot(String processdir) {
+		this.processdir = processdir;
+	}
+
+	public boolean StartProcessJavaProjects() {
+		if (processdir.equals("")) {
+			File f = new File("temp.txt");
+			if (!f.exists()) {
+				try {
+					f.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			dir = new File(f.getAbsolutePath()).getParentFile();
+			if (!dir.exists()) {
+				System.err.println("The directory which contains this jar doesn't exist??????");
+				return false;
+			}
+			f.delete();
+		} else {
+			dir = new File(processdir);
+		}
+		if (!dir.exists()) {
+			System.err.println("Directory not exists. Dir : " + dir.getAbsolutePath());
+			return false;
+		}
+		SourceCodeFileIteration.Initial();
+		SourceCodeFileIteration.IterateAllFilesAndWriteToOneBigFile(dir);
+		System.out.println("All Done......");
+		return true;
+	}
+
+	public void StopProcessJavaProjects() {
+		SourceCodeFileIteration.StopIterate();
+	}
+	
+
+	public static void main(String[] args) {
+		String processdir = args[0];
+		if (args.length == 2) {
+			BigDirectory.PrefixDirectory = args[1];
+		}
+		System.out.println("Java processor uses the directory : " + processdir);
+		ProgramProcessorRoot app = new ProgramProcessorRoot(processdir);
+		app.StartProcessJavaProjects();
+	}
+	
+}
