595eca36134cc2e7fbfe2f058c1bd8e7b9571b39
yangyixiaof <yangyixiaofirst@163.com>
2016-02-09 11:31:44 +0800

    small change.

--- linuxprogramprocessor/src/main/java/cn/yyx/research/language/simplified/JDTManager/JavaCode.java
+++
@@ -1,9 +0,0 @@
-package cn.yyx.research.language.simplified.JDTManager;
-
-import cn.yyx.research.language.JDTManager.NodeCode;
-
-public interface JavaCode {
-	public void AddOneMethodNodeCode(NodeCode nc);
-	public void OneSentenceEnd();
-	public boolean IsEmpty();
-}
